703134c8d57eaf69f5abc197ed9052bdec075dad
A. Henrie <103060170+ahenrie@users.noreply.github.com>
2023-08-18 00:34:26 +0000

    Create NOT Operator.cpp

---
+++ NOT Operator.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main(){
+        int a = 10, b = 5;
+
+        if (!(a > b))
+            printf("b is greater then a\n");
+        else
+            printf("a is greater then b\n");
+
+        return 0;
+}
