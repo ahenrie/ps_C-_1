21d03d04c713d749a195606966b17ac31c86caab
A. Henrie <103060170+ahenrie@users.noreply.github.com>
2023-08-18 00:28:23 +0000

    Create Unary Operations.cpp

---
+++ Unary Operations.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main(){
+        int positiveInterger = 100;
+        int negativeInterger = -positiveInterger;
+
+        printf("Posetive Interger = %d\n", positiveInterger);
+        printf("Negative Interger = %d\n", negativeInterger);
+
+        return 0;
+}
