3882d8c2c967c0236e56224174f4aa07073d7e89
A. Henrie <103060170+ahenrie@users.noreply.github.com>
2023-08-18 00:43:13 +0000

    Create Sizeof().cpp

---
+++ Sizeof().cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main(){
+        // printing the size of double and int in bytes
+        printf("Size of double: %d\n", sizeof(double));
+        printf("Size of interger: %d\n", sizeof(int));
+
+        return 0;
+}
